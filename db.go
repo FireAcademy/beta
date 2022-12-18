95454ee37ec7c630e477dd32a27f8b2505ab990f
yakuhito <y@kuhi.to>
2022-12-17 17:44:42 -0800

    add gorm

--- db.go
+++ db.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"os"
-    "fmt"
     "time"
     "strconv"
-    "database/sql"
-    _ "github.com/lib/pq"
+
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
-var DB *sql.DB
+var DB *gorm.DB
 
 func setupDB() {
     db_conn_string := os.Getenv("DB_CONNECTION_STRING")
     if db_conn_string == "" {
-        fmt.Printf("DB_CONNECTION_STRING not specified, exiting :(\n")
+        panic("DB_CONNECTION_STRING not specified, exiting :(\n")
         return
     }
     var err error
-    DB, err = sql.Open("postgres", db_conn_string)
+    DB, err = gorm.Open(postgres.Open(db_conn_string))
     if err != nil {
         panic(err)
     }
 
-    err = DB.Ping()
+    sqlDB, err := DB.DB()
+    if err != nil {
+        panic(err)
+    }
+
+    err = sqlDB.Ping()
     if err != nil {
         panic(err)
     }
@@ -46,9 +51,9 @@ func setupDB() {
         panic(err)
     }
     // Maximum Idle Connections
-    DB.SetMaxIdleConns(max_idle_conns_i)
+    sqlDB.SetMaxIdleConns(max_idle_conns_i)
     // Maximum Open Connections
-    DB.SetMaxOpenConns(max_open_conns_i)
+    sqlDB.SetMaxOpenConns(max_open_conns_i)
     // Connection Lifetime
-    DB.SetConnMaxLifetime(30 * time.Second)
+    sqlDB.SetConnMaxLifetime(30 * time.Second)
 }
