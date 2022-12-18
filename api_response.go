c37fce39f8a2df2ec3de13c541349b78085270b8
yakuhito <y@kuhi.to>
2022-12-17 18:35:24 -0800

    retink api_response.go + get_synced_block

--- api_response.go
+++ api_response.go
@@ -4,20 +4,20 @@ import "github.com/gofiber/fiber/v2"
 
 const COST_PER_CREDIT = 420
 
-func MakeSuccessResponse(r fiber.Map, results int) fiber.Map {
+func MakeSuccessResponse(c *fiber.Ctx, r fiber.Map, results int) error {
 	r["success"] = true
 	if results < 1 {
 		results = 1
 	}
 	r["cost"] = results * COST_PER_CREDIT
 
-	return r
+	return c.Status(200).JSON(r)
 }
 
-func MakeErrorResponse(r fiber.Map, message string) fiber.Map {
-	r["success"] = false
-	r["message"] = message
-	r["cost"] = COST_PER_CREDIT
-
-	return r
+func MakeErrorResponse(c *fiber.Ctx, message string) error {
+	return c.Status(500).JSON(fiber.Map{
+		"success": false,
+		"message": message,
+		"cost": COST_PER_CREDIT,
+	})
 }
--- sync_api.go
+++ sync_api.go
@@ -6,20 +6,38 @@ import (
 
 func getPeakSyncedBlock(c *fiber.Ctx) error {
 	var sb SyncedBlock
-	DB.Order("height desc").First(&sb)
+	result := DB.Order("height desc").First(&sb)
+	if result.Error != nil {
+		return MakeErrorResponse(c, result.Error.Error())
+	}
 
-	return c.JSON(
-		MakeSuccessResponse(SyncedBlockToJSON(sb), 1),
-	);
+	return MakeSuccessResponse(c, SyncedBlockToJSON(sb), 1)
+}
+
+type GetSyncedBlockArgs struct {
+    Height uint32 `json: "height"`   
+}
+
+func getSyncedBlock(c *fiber.Ctx) error {
+	args := new(GetSyncedBlockArgs)
+	if err := c.BodyParser(args); err != nil {
+        return MakeErrorResponse(c, err.Error())
+    }
+
+	var sb SyncedBlock
+	result := DB.First(&sb, "height = ?", args.Height)
+	if result.Error != nil {
+		return MakeErrorResponse(c, result.Error.Error())
+	}
+
+	return MakeSuccessResponse(c, SyncedBlockToJSON(sb), 1)
 }
 
 func SetupSyncAPIRoutes(app *fiber.App) {
 	app.Get("/get_peak_synced_block", getPeakSyncedBlock)
 	app.Post("/get_peak_synced_block", getPeakSyncedBlock)
 
-	app.Post("/get_synced_block", func(c *fiber.Ctx) error {
-		return c.SendString("POST get_last_synced_bloc")
-	})
+	app.Post("/get_synced_block", getSyncedBlock)
 
 	app.Post("/get_synced_blocks", func(c *fiber.Ctx) error {
 		return c.SendString("POST get_last_synced_block")
